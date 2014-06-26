d3efa144376f5db562a15c3d9cf4d3c2df5fc3ca
Jake R Huhn <imasidonion@gmail.com>
2014-06-26 17:30:01 -0400

    adds correct migrations / model associations

--- feely/db/migrate/20140626212640_create_colors.rb
+++ feely/db/migrate/20140626212640_create_colors.rb
@@ -1,6 +1,8 @@
 class CreateColors < ActiveRecord::Migration
   def change
     create_table :colors do |t|
+      t.string :name
+      t.string :hex_code 
 
       t.timestamps
     end
--- feely/db/migrate/20140626212646_create_stories.rb
+++ feely/db/migrate/20140626212646_create_stories.rb
@@ -1,6 +1,8 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
+      t.string :anecdote
+      t.belongs_to :color
 
       t.timestamps
     end
