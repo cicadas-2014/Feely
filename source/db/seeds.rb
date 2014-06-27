e5611ddb095fe365299a9160e10e0c652736da16
Danielle Adams <dadams18@gmail.com>
2014-06-27 11:06:00 -0400

    adds color seeds

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+colors = ['orange', 'blue', 'yellow', 'purple', 'red', 'green', 'pink', 'black', 'white']
+color_hexes = ['#FF530D', '#1B29FF', '#FFD01B', '#530EB2', '#FF0000', '#28B20E', '#FF1BC1', '#000000', '#FFFFFF']
+
+9.times { |x| Color.create(name: colors[x], hex_code: color_hexes[x]) }
