a3712f042663a4bfca49868198a299d966792947
Danielle Adams <dadams18@gmail.com>
2014-06-27 10:42:35 -0400

    fixes gem and database errors

--- source/spec/spec_helper.rb
+++ source/spec/spec_helper.rb
@@ -17,11 +17,16 @@ RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-
+  config.use_transactional_fixtures = true
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
