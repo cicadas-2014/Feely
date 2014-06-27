94b4be1f176be3e190d3002e317a4426528a1f03
Danielle Adams <dadams18@gmail.com>
2014-06-26 20:20:10 -0400

    create factory colors for features tests

---
+++ source/spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :blue do
+    name { 'sky blue' }
+    hex_code { '#70DBDB' }
+  end
+
+  factory :purple do
+    name { 'lavender' }
+    hex_code { '#CCCCFF' }
+  end
+end
