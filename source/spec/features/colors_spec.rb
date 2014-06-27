05a9d486ab1e2393b2ef0f9df2bf682ffde1e7b5
Kevin Zhou <kz363@nyu.edu>
2014-06-27 15:51:45 -0400

    Fixed broken feature test

--- source/spec/features/colors_spec.rb
+++ source/spec/features/colors_spec.rb
@@ -1,15 +1,17 @@
+require 'pry'
 require 'spec_helper'
 describe "Colors" do
   before(:each) do
-    @colors << Color.new( name: 'purple',
-                          hex_code: '#551A8B')
+    Color.create( name: 'purple',
+                  hex_code: '#551A8B')
   end
   # let!(:purple) { create :purple }
   # let(:blue) { create :blue }
   describe "User can view colors"  do
     it "by visiting the homepage" do
       visit root_path
-      expect(page).to have_css(".colors-container")
+      binding.pry
+      expect(page).to have_css(".color-selection")
     end
 
     it "asks the user to choose a color" do
