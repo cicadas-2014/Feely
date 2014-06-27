c856a60ae49197ee5f62925d0c6393d01e6e66c9
Tim <timothyjmcclung@gmail.com>
2014-06-27 15:59:43 -0400

    fixed stories_spec

--- source/spec/features/stories_spec.rb
+++ source/spec/features/stories_spec.rb
@@ -1,14 +1,14 @@
 describe "Stories" do
   let!(:story) { create :story }
-  describe "User can view their own story" do
-    it "by submitting their story" do
-      visit stories_all_path
-      expect(page).to have_content(story.anecdote)
-    end
-  end
+  # describe "User can view their own story" do
+  #   it "by submitting their story" do
+  #   end
+  # end
 
   describe "User can view all stories" do
     it "when redirected to the stories page" do
+      visit stories_all_path
+      expect(page).to have_content(story.anecdote)
     end
   end
 end
