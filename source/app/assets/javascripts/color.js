a6d7e9bf12ffa819a1632e6fee8622b8ede2a2e6
Danielle Adams <dadams18@gmail.com>
2014-06-28 14:38:15 -0400

    consolidate and refactor for styling

--- source/app/assets/javascripts/color.js
+++ source/app/assets/javascripts/color.js
@@ -1,18 +1,13 @@
-var speed = 500
-
-
+var speed = 350;
 
 $(document).ready(function() {
 	$('.color-selection').mouseenter(function() {
-	var color = $(this).css("background-color");
-	$('.container').css('background-color', color)
-	$('#background').fadeTo(speed,0.8);
-});
+		var color = $(this).css("background-color");
+		$('.container').css('background-color', color)
+		$('#background').fadeTo(speed, 0.8);
+	});
 	$('.color-selection').mouseleave(function() {
-		$('#background').fadeTo(speed,1);
+		$('#background').fadeTo(speed, 1.0);
 		$('.container').css('background-color', '#FFF')
-});
-
-
-
+	});
 });
