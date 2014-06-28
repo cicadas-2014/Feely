d2cc10491cbcea5dbe60b7871f74e2f0ef663c92
Danielle Adams <dadams18@gmail.com>
2014-06-28 13:52:54 -0400

    change background on landing page

--- source/app/assets/javascripts/color.js
+++ source/app/assets/javascripts/color.js
@@ -5,11 +5,12 @@ var speed = 500
 $(document).ready(function() {
 	$('.color-selection').mouseenter(function() {
 	var color = $(this).css("background-color");
-	$('.container').css('background-color',color)
+	$('.container').css('background-color', color)
 	$('#background').fadeTo(speed,0.8);
 });
 	$('.color-selection').mouseleave(function() {
 		$('#background').fadeTo(speed,1);
+		$('.container').css('background-color', '#FFF')
 });
 
 
